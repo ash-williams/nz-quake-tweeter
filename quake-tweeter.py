bbfef50a6768a5aace4f94006c884c008bb36084
Ashley Williams <ash.03@hotmail.com>
2016-06-10 12:11:25 -1200

    Added time checks, will only tweet quakes that happened in the last five minutes.

--- quake-tweeter.py
+++ quake-tweeter.py
@@ -43,38 +43,105 @@ def checkFeed():
         log(time.strftime("%c") + ": Error - retrieving feed")
         return False
 
-def tweet(e):
-    print e.title
-    try:
-        title = e.title.split(', ')
-        tweet_str = title[0] + ' earthquake hits ' + title[3] + ' on ' + title[1] + ', ' + title[2]
-    except:
-        log(time.strftime("%c") + ": Warning - title not formatted properly")
-        tweet_str = title[0]
-        
-    print tweet_str
+def convertMonthToNum(monthStr):
+    if monthStr == 'Jan':
+        month = 1
+    elif monthStr == 'Feb':
+        month = 2
+    elif monthStr == 'Mar':
+        month = 3
+    elif monthStr == 'Apr':
+        month = 4
+    elif monthStr == 'May':
+        month = 5
+    elif monthStr == 'Jun':
+        month = 6
+    elif monthStr == 'Jul':
+        month = 7
+    elif monthStr == 'Aug':
+        month = 8
+    elif monthStr == 'Sep':
+        month = 9
+    elif monthStr == 'Oct':
+        month = 10
+    elif monthStr == 'Nov':
+        month = 11
+    elif monthStr == 'Dec':
+        month = 12
+    else:
+        month = -1
+    
+    return month
+
+def send_tweet(tweet_str):
+    # print tweet_str
     config = json.load(open('./config.json'))
     
-    # try:
-    twitter = Twython(
-        config['twitter']['app_key'],
-        config['twitter']['app_secret'],
-        config['twitter']['oauth_token'],
-        config['twitter']['oauth_token_secret'])
+    try:
+        twitter = Twython(
+            config['twitter']['app_key'],
+            config['twitter']['app_secret'],
+            config['twitter']['oauth_token'],
+            config['twitter']['oauth_token_secret'])
+    
+        twitter.update_status(status=tweet_str)
+    except:
+        log(time.strftime("%c") + ": Error - failed to send tweet")
 
-    twitter.update_status(status=tweet_str)
-    # except:
-    #     log(time.strftime("%c") + ": Error - failed to send tweet")
+def tweet(e):
+    # print e.title
+    try:
+        title = e.title.split(', ')
+        
+        import datetime as dt
+        import os
+        os.environ['TZ'] = 'NZ'
+        time.tzset()
+        current = time.strftime("%c").split()
+        currentTime = current[3].split(':')
+        year = int(current[4])
+        month = convertMonthToNum(current[1])
+        day = int(current[2])
+        hour = int(currentTime[0])
+        minute = int(currentTime[1])
+        second = int(currentTime[2])
+        # print year, month, day, hour, minute, second
+        curr_date = dt.datetime(year,month,day,hour,minute,second)
+        
+        quake = title[2].split()
+        quakeTime = quake[4].split(':')
+        year = int(quake[2])
+        month = convertMonthToNum(quake[0])
+        day = int(quake[1])
+        hour = int(quakeTime[0])
+        minute = int(quakeTime[1])
+        second = int(quakeTime[2])
+        # print year, month, day, hour, minute, second
+        quake_date = dt.datetime(year,month,day,hour,minute,second)
+        
+        time_difference = (curr_date - quake_date).total_seconds()
+        
+        # print time_difference
+        
+        if time_difference <= 300:
+            send_tweet(title[0] + ' earthquake hits ' + title[3] + ' on ' + title[1] + ', ' + title[2])
+        
+    except:
+        log(time.strftime("%c") + ": Warning - troubles formatting or the timing is off")
 
-initialize()
+def main():
+    initialize()
+    
+    while(True):
+        d = checkFeed()
+        if d != False:
+            for e in d.entries:
+                if e.id == previous_entry:
+                    break
+                else:
+                    tweet(e)
+        time.sleep(60)
 
-while(True):
-    d = checkFeed()
-    if d != False:
-        for e in d.entries:
-            if e.id == previous_entry:
-                break
-            else:
-                tweet(e)
-    time.sleep(60)
+if __name__ == '__main__':
+    main()
         
